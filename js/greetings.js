57f27494ef53dbb58e985c68c89b54cc1a566aff
GrassHopper42 <sunsr007@naver.com>
2021-12-28 21:00:42 +0900

    add basic css

--- js/greetings.js
+++ js/greetings.js
@@ -1,11 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const todoForm = document.querySelector("#todo-form");
+const todoList = document.querySelector("#todo-list");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+function turnOn() {
+  todoForm.classList.remove(HIDDEN_CLASSNAME);
+  todoList.classList.remove(HIDDEN_CLASSNAME);
+}
 const paintingGreetings = (username) => {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
@@ -18,8 +24,10 @@ if (savedUsername === null) {
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
     localStorage.setItem(USERNAME_KEY, username);
+    turnOn();
     paintingGreetings(username);
   });
 } else {
+  turnOn();
   paintingGreetings(savedUsername);
 }
